e61d1c0f28219f11f6a150c0260f27e4a275d421
Yan Duarte <yduarte.ctr@touchbistro.com>
2023-08-18 19:18:34 -0300

    Added fallback to events when starting the service

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,54 @@
 import express from "express";
 import { json } from "body-parser";
 import cors from "cors";
-import { Posts } from "./types";
+import axios from "axios";
+import {
+  Event,
+  EventCommentCreated,
+  EventCommentUpdated,
+  EventPostCreated,
+  Posts,
+} from "./types";
 
 const app = express();
 app.use(json());
 app.use(cors());
 
 const posts: Posts = {};
+
+const handleEvent = ({ type, data }: Event) => {
+  switch (type) {
+    case "PostCreated":
+      const { id, title } = data as EventPostCreated;
+
+      posts[id] = { id: id, title, comments: posts?.[id]?.comments ?? [] };
+      break;
+    case "CommentCreated": {
+      const { postId, ...comment } = data as EventCommentCreated;
+
+      posts[postId].comments.push(comment);
+      break;
+    }
+    case "CommentUpdated": {
+      const {
+        postId,
+        id: commentId,
+        ...eventComment
+      } = data as EventCommentUpdated;
+
+      const localComment = posts[postId].comments.find(
+        (comment) => comment.id === commentId
+      );
+
+      if (localComment) {
+        localComment.content = eventComment.content;
+        localComment.status = eventComment.status;
+      }
+
+      break;
+    }
+  }
+};
 
 app.get("/posts", (req, res) => {
   res.send(posts);
@@ -18,24 +59,20 @@ app.post("/events", async (req, res) => {
 
   console.log("Received Event", { type, data });
 
-  switch (type) {
-    case "PostCreated":
-      const { id, title } = data;
-
-      posts[id] = { id: id, title, comments: posts?.[id]?.comments ?? [] };
-      break;
-    case "CommentCreated":
-      const { id: commentId, content, postId } = data;
-
-      posts[postId].comments.push({ id: commentId, content });
-      break;
-  }
-
-  console.log(posts);
+  handleEvent({ type, data });
 
   res.send({});
 });
 
-app.listen(4002, () => {
+app.listen(4002, async () => {
   console.log("Listening on 4002");
+
+  const { data: events } = await axios.get<Event[]>(
+    "http://localhost:4005/events"
+  );
+
+  events.forEach((event) => {
+    console.log("Processing event: ", event.type);
+    handleEvent(event);
+  });
 });
--- src/types.ts
+++ src/types.ts
@@ -5,6 +5,27 @@ export interface Posts {
     comments: {
       id: string;
       content: string;
+      status: "pending" | "approved" | "rejected";
     }[];
   };
 }
+
+export interface Event {
+  type: string;
+  data: Record<string, unknown>;
+}
+
+export type EventPostCreated = {
+  id: string
+  title: string
+}
+
+export type EventCommentCreated = {
+  id: string;
+  postId: string;
+  content: string;
+  status: "pending" | "approved" | "rejected";
+};
+
+
+export type EventCommentUpdated = EventCommentCreated
